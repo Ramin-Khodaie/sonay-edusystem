6a80b348ac56c33679ea544b30530a0979bce23b
aysan eshraghi <eshraghi.aysan@gmail.com>
2022-10-04 16:58:15 +0330

    blah

--- nodeserver/src/components/Dashboard/TeacherDashboard/TeacherCourse.jsx
+++ nodeserver/src/components/Dashboard/TeacherDashboard/TeacherCourse.jsx
@@ -1,24 +1,40 @@
-import { SimpleGrid, Text } from '@chakra-ui/react'
-import TeacherCourseDetail from '../TeacherCourseDetail'
+import { Alert, AlertIcon, SimpleGrid, Text } from "@chakra-ui/react";
+import TeacherCourseDetail from "../TeacherCourseDetail";
 
+const TeacerCourse = (props) => {
+  const { data } = props;
+  return (
+    <>
+      {data.length === 0 ? (
+        <Alert
+          borderRadius={"3rem"}
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          height="100px"
+          status="warning"
+        >
+          <AlertIcon />
+          <Text textAlign={"center"}>هیچ کلاسی برای شما تعریف نشده است</Text>
+        </Alert>
+      ) : (
+        <SimpleGrid
+          dir="rtl"
+          columns={{ sm: 1, md: 2, xl: 3 }}
+          spacing="24px"
+          mb="20px"
+        >
+          {data.map((item, key) => (
+            <TeacherCourseDetail
+              name={item.courses.name}
+              students={item.students}
+            />
+          ))}
+        </SimpleGrid>
+      )}
+    </>
+  );
+};
 
-const TeacerCourse = (props)=>{
-    const {data} = props
-    return(<>
-
-{data.length === 0 ? 
-    <Text>نووو</Text> : 
-    <SimpleGrid dir='rtl' columns={{ sm: 1, md:2 , xl: 3 }} spacing="24px" mb="20px" >
- {
-        data.map((item , key)=>(
-            <TeacherCourseDetail name={item.courses.name} students={item.students} />
-        ))
-    }
-    </SimpleGrid>}
-    
-   
-    
-    </>)
-}
-
-export default TeacerCourse
+export default TeacerCourse;
